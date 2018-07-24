a55ab13875d3b4fda2f053d53494fa31906edde4
cyberboy1551 <blacy1551@gmail.com>
2018-07-24 16:17:31 -0500

    SocketIO
    Created Socket Io support for the plane game. There is very little functionality, however, it works. You have connect to the server and send messages to and from unity from the clinician view...and vica versa.

---
+++ Assets/Resources/Scripts/ClientController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SocketIO;
+
+public class ClientController : MonoBehaviour {
+    [SerializeField]
+    public SocketIOComponent socket;
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(ConnectToServer());
+        socket.On("USER_CONNECTED", OnUserConnected);
+        socket.On("forUnity", getData);
+	}
+    IEnumerator ConnectToServer()
+    {
+        yield return new WaitForSeconds(0.5f);
+        socket.Emit("USER_CONNECT");
+        Debug.Log("User Connected");
+        yield return new WaitForSeconds(0.5f);
+        Dictionary<string, string> data = new Dictionary<string, string>();
+        data["name"] = "User" + Random.Range(0, 9999);
+        data["type"] = "Unity";
+
+        socket.Emit("GETDATA", new JSONObject(data));
+    }
+    private void OnUserConnected (SocketIOEvent evt)
+    {
+        Debug.Log("Client Connected: " + evt.data);
+    }
+    public void getData(SocketIOEvent msg)
+    {
+        Debug.Log("Got data from server" + msg.data); 
+    }
+	
+}
